5db80c607ff5bc23d0f7502ecad53386db0a4763
Anim Mouse <git@animmouse.com>
2025-01-25 16:07:37 +0800

    Update PowerShell script

--- PowerShell/FFmpeg/FFmpeg batch encode to AV1.ps1
+++ PowerShell/FFmpeg/FFmpeg batch encode to AV1.ps1
@@ -1,7 +1,8 @@
 Get-ChildItem -Filter *.mp4 | ForEach-Object {
   $output = $_.Basename
-  ffmpeg -hide_banner -i "$_" -c:v libsvtav1 `
+  ffmpeg -hide_banner -i "$_" -map_metadata 0 -c:v libsvtav1 `
   -preset 6 -crf 34 -pix_fmt yuv420p10le `
   -svtav1-params "tune=0:keyint=10s:enable-overlays=1" `
   -c:a libopus -b:a 64k "$output.mkv"
+  (Get-Item "$output.mkv").LastWriteTime = (Get-Item "$_").LastWriteTime
   }
